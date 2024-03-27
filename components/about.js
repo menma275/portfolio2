691c5ab2b6f67c64ee8399eab4bb42dcc6ebd742
menma275 <ohsusie9@gmail.com>
2024-03-27 14:59:22 +0900

    blueskyリンク更新

--- components/about.js
+++ components/about.js
@@ -3,6 +3,7 @@ import portrait from "../public/img/me.png";
 
 import { FaXTwitter } from "react-icons/fa6";
 import { FaGithub, FaLinkedin, FaSquare } from "react-icons/fa";
+import Bluesky from "../public/img/BlueSky_logo.svg";
 
 const timeline = [
   {
@@ -27,7 +28,11 @@ export default function About() {
   return (
     <div className="my-5">
       <div className="my-5 flex flex-row gap-5">
-        <Image src={portrait} alt="Portrait" className="w-[75px] h-[75px] rounded-full" />
+        <Image
+          src={portrait}
+          alt="Portrait"
+          className="w-[75px] h-[75px] rounded-full"
+        />
         <div className="m-0 my-auto">
           <h1 className="mb-1 text-lg font-bold">Kusuke SAKAMURA</h1>
           <div className="flex flex-wrap gap-3">
@@ -48,10 +53,11 @@ export default function About() {
                 </span>
               </a>
               <a
-                href="https://bsky.app/profile/sakamura.bsky.social"
+                href="https://bsky.app/profile/sakamura.dev"
                 className="hover:text-[var(--accent)] transition-all duration-300 group relative"
               >
                 <FaSquare />
+                {/* <Image src={Bluesky} alt="BlueSky" className="w-5 h-5" /> */}
                 <span className="opacity-0 invisible group-hover:visible group-hover:opacity-100 text-xs absolute left-0 -bottom-6 px-1 py-0.5rounded-md text-[var(--font-secondary)] transition-all duration-300">
                   BlueSky
                 </span>
@@ -80,8 +86,11 @@ export default function About() {
       </div>
       <h1 className="mb-2 text-lg">About</h1>
       <p className="text-normal font-light">
-        Hi, I&apos;m Kusuke SAKAMURA. I&apos;m a generative artist, web developer, and designer.
-        I&apos;m currently a student at the Tokyo Metropolitan University studying Art Engineering. I&apos;m interested in the intersection of art and technology, and I&apos;m always looking for new ways to express mycreativity.
+        Hi, I&apos;m Kusuke SAKAMURA. I&apos;m a Generative Artist, Web
+        Developer, and Designer. I&apos;m currently a student at the Tokyo
+        Metropolitan University studying Art Engineering. I&apos;m interested in
+        the intersection of art and technology, and I&apos;m always looking for
+        new ways to express my creativity.
       </p>
       <div className="mt-3">
         {timeline.map((data, index) => (
